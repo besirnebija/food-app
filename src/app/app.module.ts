e147fecc5a801e8c2127ecbcee7b8de238612605
Besir Nebija <besirnebiu@gmail.com>
2024-12-10 13:39:48 +0100

    Final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './search/search.component';
+import { RatingModule } from 'ng-starrating';
+import { TagsComponent } from './tags/tags.component';
+import { FoodPageComponent } from './food-page/food-page.component';
+import { CartPageComponent } from './cart-page/cart-page.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    SearchComponent,
+    TagsComponent,
+    FoodPageComponent,
+    CartPageComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RatingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { FoodService } from '../services/food/food.service';
+import { Food } from '../shared/models/Food';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  
+  foods: Food[] = [];
+
+  constructor( private foodService: FoodService, private route: ActivatedRoute ){}
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      if(params['searchTerm']) //if params['searchTerm'] is not undefined
+        this.foods = this.foodService.getAllFoodsBySearchterm(params['searchTerm'])  //filter the foods
+      else if(params['tag'])  //else if params['tag'] is not undefined 
+        this.foods = this.foodService.getAllFoodsByTag(params['tag']) 
+      else 
+      this.foods = this.foodService.getAll(); //get all foods
+    })
+  }
+}
